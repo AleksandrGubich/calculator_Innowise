ecf1bf86020e75ca16685e06d55fef1663eaa554
Aleksandr Gubich <a.gubich@bk.ru>
2024-10-06 13:16:35 +0300

    Final project deployment

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 
   module: {
@@ -37,5 +38,5 @@ module.exports = {
     },
   },
 
-  mode: 'development',
+  mode: 'production',
 };
